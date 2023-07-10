72741f89275f124a93aff603fdf9aec624e22999
Connor Dillon <connor@connordillon.dev>
2023-07-10 15:34:20 -0700

    More init

--- src/frontend/client/App.tsx
+++ src/frontend/client/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World!!!</h1>
     </>
   );
 }
