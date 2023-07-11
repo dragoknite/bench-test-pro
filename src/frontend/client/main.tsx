4308832ad97bb44a44d8593f5c7717cc306e07fa
paranoidFrappe <delectable999@gmail.com>
2023-07-11 09:24:38 -0700

    for back up to dev branch

--- src/frontend/client/main.tsx
+++ src/frontend/client/main.tsx
@@ -1,3 +1,14 @@
+// import React from "react";
+// import {createRoot} from "react-dom/client";
+// import App from "./App";
+
+
+
+// let root = createRoot(document.getElementById("root") as HTMLElement)
+
+// root.render(
+//     <App/>
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -8,4 +19,4 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App/>
   </React.StrictMode>
-);
+); 
